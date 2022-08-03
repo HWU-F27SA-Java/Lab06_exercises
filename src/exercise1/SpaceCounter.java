c5ef1308e3422f1e31a0448e413d892139ecd86f
Batatia, Hadj <69246983+hbatatia@users.noreply.github.com>
2022-08-03 14:32:12 +0100

    Create SpaceCounter.java

---
+++ src/exercise1/SpaceCounter.java
@@ -0,0 +1,14 @@
+package exercise1;
+
+import java.util.Scanner;
+
+public class SpaceCounter {
+
+	public static void main(String[] args) {
+        System.out.print("Enter a text ...");
+        Scanner scanner = new Scanner(System.in);
+ //your code here
+        scanner.close();
+	}
+
+}
