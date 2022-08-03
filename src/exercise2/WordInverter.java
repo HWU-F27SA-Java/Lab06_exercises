105f37ccfeb1200d0cc08ba91faa7a0b07123bda
Batatia, Hadj <69246983+hbatatia@users.noreply.github.com>
2022-08-03 14:32:17 +0100

    Create WordInverter.java

---
+++ src/exercise2/WordInverter.java
@@ -0,0 +1,16 @@
+package exercise2;
+
+import java.util.Scanner;
+import exercise1.Sentence;
+
+public class WordInverter {
+
+	public static void main(String[] args) {
+	       System.out.print("Enter a sentence ...");
+	       Scanner scanner = new Scanner(System.in);
+
+//your code here
+	        scanner.close();
+	}
+
+}
