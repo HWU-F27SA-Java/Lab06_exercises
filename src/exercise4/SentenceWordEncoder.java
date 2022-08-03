2b200f37a521eedbc6b9898f7cd8b5c11d7eadac
Batatia, Hadj <69246983+hbatatia@users.noreply.github.com>
2022-08-03 14:32:24 +0100

    Create SentenceWordEncoder.java

---
+++ src/exercise4/SentenceWordEncoder.java
@@ -0,0 +1,20 @@
+package exercise4;
+
+import java.util.Random;
+import java.util.Scanner;
+
+import exercise1.Sentence;
+
+
+public class SentenceWordEncoder {
+
+	public static void main(String[] args) {
+		System.out.print("Do you want to encode (e/E) or decode (d/D)? ");
+
+		System.out.print("Enter a string ...");
+//your code here
+		
+
+	}
+
+}
