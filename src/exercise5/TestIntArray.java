4b0ce35fd89d9d51a3c6bb7447f9e68ef3b95421
Batatia, Hadj <69246983+hbatatia@users.noreply.github.com>
2022-08-03 14:32:28 +0100

    Create TestIntArray.java

---
+++ src/exercise5/TestIntArray.java
@@ -0,0 +1,15 @@
+package exercise5;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class TestIntArray {
+
+	public static void main(String[] args) {
+        Random r = new Random();
+        System.out.print("Enter the size of the array: ");
+//your code here
+
+	}
+
+}
